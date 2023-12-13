7b2f3a7f57b438024bbed530f92dca31cf023bba
Zachary Tully <107644545+ZackTully@users.noreply.github.com>
2023-12-12 17:50:23 -0700

    Feature/herc battery controller (#13)
    * Updating readme to provide rough details of WHOC.
    
    * Updating URL link to main repo.
    
    * First-pass implementation. Not yet tested.
    
    * Update to match ROSCO example 17.
    
    * outline python server added.
    
    * Architecture for base class and inheriting subclasses.
    
    * remove unnecessary zmq content.
    
    * Actuator disk direct connection
    
    * Changing ,server, to ,interface,
    
    * interface now passed in on instantiation.
    
    * Interfaces now pass through the controller modules.
    
    * Working on a hercules direct call demo.
    
    * Runs.
    
    * Now plotting.
    
    * Basic readme for interfaces.
    
    * Renaming interfaces somewhat; adding example to repo.
    
    * hercules battery controller skeleton
    
    * formatting discrepancies
    
    * update example batch_script
    
    * delete erronious input arguments
    
    ---------
    
    Co-authored-by: misha <msinner@nrel.gov>
    Co-authored-by: Zachary <ztully@nrel.gov>
    Co-authored-by: misi9170 <39596329+misi9170@users.noreply.github.com>

--- whoc/controller_base.py
+++ whoc/controller_base.py
@@ -1,13 +1,8 @@
 from abc import abstractmethod
 
 
-class ControllerBase():
-
-    def __init__(self,
-        interface,
-        verbose=True
-        ):
-
+class ControllerBase:
+    def __init__(self, interface, verbose=True):
         self._s = interface
         self.verbose = verbose
 
@@ -16,10 +11,10 @@ class ControllerBase():
         #         "HELICS interface has not yet been implemented."
         #     )
 
-        #     # TODO: eventually, this would set up a federate (with same 
+        #     # TODO: eventually, this would set up a federate (with same
         #     # public methods as the whoc_zmq_server
         #     #self._s = whoc_helics_federate()
-        
+
         # elif use_zmq_interface:
         #     from servers.zmq_server import WHOC_zmq_server
 
@@ -46,15 +41,13 @@ class ControllerBase():
         return None
 
     def send_setpoints(self, dict=None):
-
         self._s.check_setpoints(self.setpoints_dict)
         dict = self._s.send_setpoints(dict, **self.setpoints_dict)
 
-        return dict # or main_dict, or what?
+        return dict  # or main_dict, or what?
 
     def step(self, hercules_dict=None):
-
-        # If not running with direct hercules integration, 
+        # If not running with direct hercules integration,
         # hercules_dict may simply be None throughout this method.
         self.receive_measurements(hercules_dict)
 
@@ -62,8 +55,8 @@ class ControllerBase():
 
         hercules_dict = self.send_setpoints(hercules_dict)
 
-        return hercules_dict # May simply be None.
-    
+        return hercules_dict  # May simply be None.
+
     @abstractmethod
     def compute_setpoints(self):
         # Control algorithms should be implemented in the compute_setpoints
