b481b1a4682a8e43c58a8d22eb676c65907851ca
misi9170 <39596329+misi9170@users.noreply.github.com>
2023-12-20 17:46:16 -0500

    Rename amr-standin in examples (#23)
    * runscripts now call correct hercules packages and run.
    
    * Rename files for standin.

--- examples/simple_yaw_control_amrstandin/hercules_runscript_amr_standin.py
+++ examples/simple_yaw_control_amrstandin/hercules_runscript_amr_standin.py
@@ -14,16 +14,16 @@
 
 import sys
 
-from hercules.dummy_amr_wind import launch_dummy_amr_wind
+from hercules.amr_wind_standin import launch_amr_wind_standin
 
 # Check that one command line argument was given
 if len(sys.argv) != 2:
-    raise Exception("Usage: python emu_runscript_dummy_amr.py <amr_input_file>")
+    raise Exception("Usage: python hercules_runscript_amr_standin.py <amr_input_file>")
 
 # # Get the first command line argument
 # This is the name of the file to read
 amr_input_file = sys.argv[1]
-print(f"Running AMR-Wind dummy with input file: {amr_input_file}")
+print(f"Running AMR-Wind standin with input file: {amr_input_file}")
 
 
-launch_dummy_amr_wind(amr_input_file)
+launch_amr_wind_standin(amr_input_file)
