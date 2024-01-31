Hercules units now fixed as kW.

examples/lookup-based_wake_steering_florisstandin/plot_output_data.py
@@ -28,12 +28,12 @@ for t in range(n_turbines):
     ax[0].plot(time, yaws[:,t], color=line.get_color(), label="T{0:03d} yaw pos.".format(t),
         linestyle=":")
     if t == 0:
-        ax[1].fill_between(time, powers[:,t]/1e3, color=line.get_color(),
+        ax[1].fill_between(time, powers[:,t], color=line.get_color(),
             label="T{0:03d} power".format(t))
     else:
-        ax[1].fill_between(time, powers[:,:t+1].sum(axis=1)/1e3, powers[:,:t].sum(axis=1)/1e3,
+        ax[1].fill_between(time, powers[:,:t+1].sum(axis=1), powers[:,:t].sum(axis=1),
             color=line.get_color(), label="T{0:03d} power".format(t))
-ax[1].plot(time, powers.sum(axis=1)/1e3, color="black", label="Farm power")
+ax[1].plot(time, powers.sum(axis=1), color="black", label="Farm power")
 
 # Plot aesthetics
 ax[0].grid()
@@ -46,6 +46,8 @@ ax[1].grid()
 ax[1].set_ylabel("Power [kW]")
 ax[1].set_xlabel("Time [s]")
 ax[1].legend(loc="lower left")
+
+#fig.savefig("../../docs/graphics/lookup-table-example-plot.png", dpi=300, format="png")
 
 # Almost equal power to begin with as turbines turbines are not aligned. As the wind direction
 # shifts towards the aligned direction beginning at t = 30s, the downstream turbine (T001) begins to