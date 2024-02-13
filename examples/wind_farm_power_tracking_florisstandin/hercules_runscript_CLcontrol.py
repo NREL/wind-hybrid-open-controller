29ca132dd5efb0e2f9dc92cd8b14131a6ba1c700
misi9170 <michael.sinner@nrel.gov>
2024-02-12 17:42:58 -0700

    plotting; use output_file option in input_dict.

--- examples/wind_farm_power_tracking_florisstandin/hercules_runscript_CLcontrol.py
+++ examples/wind_farm_power_tracking_florisstandin/hercules_runscript_CLcontrol.py
@@ -24,6 +24,7 @@ from whoc.controllers.wind_farm_power_tracking_controller import (
 from whoc.interfaces.hercules_actuator_disk_interface import HerculesADInterface
 
 input_dict = load_yaml(sys.argv[1])
+input_dict["output_file"] = "hercules_output_cl.csv"
 
 interface = HerculesADInterface(input_dict)
 
--- examples/wind_farm_power_tracking_florisstandin/plot_output_data.py
+++ examples/wind_farm_power_tracking_florisstandin/plot_output_data.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("hercules_output.csv")
-
-print(df.columns)
+dfs = [pd.read_csv("hercules_output_ol.csv"), pd.read_csv("hercules_output_cl.csv")]
+labels = ["Open-loop control", "Closed-loop control"]
 
 n_turbines = 2
 wf_str = "hercules_comms.amr_wind.wind_farm_0."
@@ -12,40 +11,42 @@ pow_cols = [wf_str+"turbine_powers.{0:03d}".format(t) for t in range(n_turbines)
 wd_cols = [wf_str+"turbine_wind_directions.{0:03d}".format(t) for t in range(n_turbines)]
 yaw_cols = [wf_str+"turbine_yaw_angles.{0:03d}".format(t) for t in range(n_turbines)]
 
-# Extract data from larger array
-time = df.dt.values * np.arange(0, len(df), 1)
-powers = df[pow_cols].to_numpy()
-wds = df[wd_cols].to_numpy()
-yaws = df[yaw_cols].to_numpy()
+for df, l in zip(dfs, labels):
+    # Extract data from larger array
+    time = df.dt.values * np.arange(0, len(df), 1)
+    powers = df[pow_cols].to_numpy()
+    wds = df[wd_cols].to_numpy()
+    yaws = df[yaw_cols].to_numpy()
 
-# Plots
-fig, ax = plt.subplots(2, 1, sharex=True)
-fig.set_size_inches(10, 5)
+    # Plots
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    fig.set_size_inches(10, 5)
 
-# Direction
-for t in range(n_turbines):
-    line, = ax[0].plot(time, wds[:,t], label="T{0:03d} wind dir.".format(t))
-    ax[0].plot(time, yaws[:,t], color=line.get_color(), label="T{0:03d} yaw pos.".format(t),
-        linestyle=":")
-    if t == 0:
-        ax[1].fill_between(time, powers[:,t], color=line.get_color(),
-            label="T{0:03d} power".format(t))
-    else:
-        ax[1].fill_between(time, powers[:,:t+1].sum(axis=1), powers[:,:t].sum(axis=1),
-            color=line.get_color(), label="T{0:03d} power".format(t))
-ax[1].plot(time, powers.sum(axis=1), color="black", label="Farm power")
+    # Direction
+    for t in range(n_turbines):
+        line, = ax[0].plot(time, wds[:,t], label="T{0:03d} wind dir.".format(t))
+        ax[0].plot(time, yaws[:,t], color=line.get_color(), label="T{0:03d} yaw pos.".format(t),
+            linestyle=":")
+        if t == 0:
+            ax[1].fill_between(time, powers[:,t], color=line.get_color(),
+                label="T{0:03d} power".format(t))
+        else:
+            ax[1].fill_between(time, powers[:,:t+1].sum(axis=1), powers[:,:t].sum(axis=1),
+                color=line.get_color(), label="T{0:03d} power".format(t))
+    ax[1].plot(time, powers.sum(axis=1), color="black", label="Farm power")
 
-# Plot aesthetics
-ax[0].grid()
-ax[0].set_xlim([time[0], time[-1]])
-ax[0].set_ylim([240, 290])
-ax[0].set_ylabel("Direction [deg]")
-ax[0].legend(loc="lower left")
+    # Plot aesthetics
+    ax[0].grid()
+    ax[0].set_xlim([time[0], time[-1]])
+    ax[0].set_ylim([240, 290])
+    ax[0].set_ylabel("Direction [deg]")
+    ax[0].legend(loc="lower left")
+    ax[0].set_title(l)
 
-ax[1].grid()
-ax[1].set_ylabel("Power [kW]")
-ax[1].set_xlabel("Time [s]")
-ax[1].legend(loc="lower left")
+    ax[1].grid()
+    ax[1].set_ylabel("Power [kW]")
+    ax[1].set_xlabel("Time [s]")
+    ax[1].legend(loc="lower left")
 
 #fig.savefig("../../docs/graphics/lookup-table-example-plot.png", dpi=300, format="png")
 
