82d44fe67145ef01df485f275365c2bcb1ad3986
Aoife Henry <aoife.henry@colorado.edu>
2024-04-01 16:18:09 -0600

    updates

--- whoc/case_studies/bash_script.sh
+++ whoc/case_studies/bash_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=floris_case_studies.py
 #SBATCH --time=6:00:00
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=7
+#SBATCH --ntasks-per-node=104
 #SBATCH --account=ssc
 
 module purge
