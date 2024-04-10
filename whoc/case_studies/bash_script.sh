a042ec97c9496294a3ffdd09e71e714945ce37a2
Aoife Henry <ahenry@kl1.head.cm.kestrel.hpc.nrel.gov>
2024-04-10 09:39:30 -0600

    changed gitignore

--- whoc/case_studies/bash_script.sh
+++ whoc/case_studies/bash_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=floris_case_studies.py
-#SBATCH --time=12:00:00
-#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --nodes=7
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=104
 #SBATCH --account=ssc
 
 module purge
 module load conda
-module load openmpi/4.0.0
+module load openmpi
 conda activate whoc
 
 mpirun -np $SLURM_NTASKS python -m mpi4py.futures run_case_studies.py
