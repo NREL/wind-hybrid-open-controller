75f766bc330b274230a2b3d05218b903cfb5e6b0
Aoife Henry <ahenry@kl2.head.cm.kestrel.hpc.nrel.gov>
2024-04-20 10:24:08 -0600

    updated bash scripts

--- whoc/case_studies/bash_script_debug.sh
+++ whoc/case_studies/bash_script_debug.sh
@@ -2,14 +2,16 @@
 #SBATCH --job-name=debug_floris_case_studies.py
 #SBATCH --time=01:00:00
 #SBATCH --partition=debug
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=104
 #SBATCH --account=ssc
 
 module purge
 module load conda
 module load openmpi
 conda activate whoc
-echo $SLURM_NTASKS
-srun -n $SLURM_NTASKS python -m mpi4py.futures run_case_studies.py debug mpi 7
+
+# srun python run_case_studies.py debug nompi 0 1 2 3 4 5 6 7
+mpirun -np $SLURM_NTASKS python -m mpi4py.futures run_case_studies.py debug mpi 0 1 2 3 4 5 6 7
 # srun python run_case_studies.py
