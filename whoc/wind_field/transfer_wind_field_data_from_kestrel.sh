fd853cc50814160945369e1322d258f4844a7a7d
Aoife Henry <aoife.henry@colorado.edu>
2024-04-22 09:45:52 -0600

    updated gitignore

---
+++ whoc/wind_field/transfer_wind_field_data_from_kestrel.sh
@@ -0,0 +1,1 @@
+scp -r ahenry@kestrel.hpc.nrel.gov:/home/ahenry/toolboxes/whoc_env/wind-hybrid-open-controller/examples/post_processing /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/post_processing
