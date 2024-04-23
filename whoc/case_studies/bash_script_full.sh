f65db782c36a7c5ab2d66a547f6cb2618e2feeb3
Aoife Henry <ahenry@kl1.head.cm.kestrel.hpc.nrel.gov>
2024-04-22 20:45:27 -0600

    kestrel updates

--- whoc/case_studies/bash_script_full.sh
+++ whoc/case_studies/bash_script_full.sh
@@ -6,10 +6,12 @@
 #SBATCH --account=ssc
 
 module purge
+#module load cray-python 
 module load conda
-module load openmpi
+#module load openmpi
 conda activate whoc
 
+#python run_case_studies.py nodebug nompi parallel 0 1 2 3 4 5 6 7
 # srun python run_case_studies.py debug nompi 0 1 2 3 4 5 6 7
-srun -n $SLURM_NTASKS python -m mpi4py.futures run_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
+srun -n $SLURM_NTASKS python run_case_studies.py nodebug dask parallel 0 1 2 3 4 5 6 7
 # srun python run_case_studies.py
