410bd5a132203eb138bea940df500a0c60ce4d27
Aoife Henry <ahenry@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-05-02 11:16:50 -0600

    kestrel updates

--- examples/run_amr_precursor.sh
+++ examples/run_amr_precursor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=amr_precursor
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 ##SBATCH --partition=debug
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=104
@@ -22,5 +22,10 @@ spack load amr-wind+helics+openfast
 echo "Starting AMR-Wind job at: " $(date)
 echo $SLURM_NTASKS
 # Now go back to scratch folder and launch the job
-srun -n $SLURM_NTASKS /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original.inp
+srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_0.inp
+srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_1.inp
+srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_2.inp
+srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_3.inp
+srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_4.inp
+srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_5.inp
 echo "Finished AMR-Wind job at: " $(date)
