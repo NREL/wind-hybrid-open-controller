699481a4845961fe21e23fce10430268267f2f82
Aoife Henry <ahenry@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-05-06 09:13:00 -0600

    kestrel updates2

--- examples/run_amr_precursor.sh
+++ examples/run_amr_precursor.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=amr_precursor
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 ##SBATCH --partition=debug
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=104
 #SBATCH --account=ssc
 
 # A lot of modules and conda stuff
 module purge
 
-export MPICH_SHARED_MEM_COLL_OPT=mpi_bcast,mpi_barrier
-export MPICH_COLL_OPT_OFF=mpi_allreduce
+#export MPICH_SHARED_MEM_COLL_OPT=mpi_bcast,mpi_barrier
+#export MPICH_COLL_OPT_OFF=mpi_allreduce
 
 export SPACK_MANAGER="/home/ahenry/toolboxes/spack-manager"
 source $SPACK_MANAGER/start.sh
@@ -19,13 +19,19 @@ quick-activate /home/ahenry/toolboxes/whoc_env
 PATH=$PATH:/home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy
 spack load amr-wind+helics+openfast
 
-echo "Starting AMR-Wind job at: " $(date)
+rm logamr
+echo "Starting AMR-Wind job at: " $(date) >> logamr
 echo $SLURM_NTASKS
 # Now go back to scratch folder and launch the job
 srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_0.inp
+echo "Finished precursor 0 at:" $(date) >> logamr
 srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_1.inp
+echo "Finished precursor 1 at:" $(date) >> logamr
 srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_2.inp
+echo "Finished precursor 2 at:" $(date) >> logamr
 srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_3.inp
+echo "Finished precursor 3 at:" $(date) >> logamr
 srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_4.inp
+echo "Finished precursor 4 at:" $(date) >> logamr
 srun /home/ahenry/toolboxes/whoc_env/amr-wind/spack-build-bmx2pfy/amr_wind amr_precursor_original_5.inp
-echo "Finished AMR-Wind job at: " $(date)
+echo "Finished AMR-Wind job at: " $(date) >> logamr
