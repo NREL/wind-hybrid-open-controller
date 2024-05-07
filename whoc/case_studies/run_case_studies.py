24d0d920cb1e468e0f9b0bede9eb9da0f85d7137
Aoife Henry <aoife.henry@colorado.edu>
2024-05-07 13:05:59 -0600

    mpi updates

--- whoc/case_studies/run_case_studies.py
+++ whoc/case_studies/run_case_studies.py
@@ -590,7 +590,7 @@ def simulate_controller(controller_class, input_dict, **kwargs):
 
     return results_df
 
-def run_simulations(case_study_keys, regenerate_wind_field, n_seeds, run_parallel, multi):
+def initialize_simulations(case_study_keys, regenerate_wind_field, n_seeds, run_parallel, multi):
 
     input_dict = load_yaml(os.path.join(os.path.dirname(whoc_file), "../examples/hercules_input_001.yaml"))
 
@@ -701,57 +701,11 @@ def run_simulations(case_study_keys, regenerate_wind_field, n_seeds, run_paralle
     wind_field_config["regenerate_distribution_params"] = False
     wind_field_config["time_series_dt"] = int(input_dict["controller"]["dt"] // input_dict["dt"])
 
-    print(f"about to submit calls to simulate_controller")
-    if run_parallel:
-        # if platform == "linux":
-        
-        if multi == "dask":
-            futures = [client.submit(simulate_controller, 
-                                                controller_class=globals()[case_lists[c]["controller_class"]], input_dict=d, 
-                                                wind_case_idx=case_lists[c]["wind_case_idx"], wind_mag_ts=wind_mag_ts[case_lists[c]["wind_case_idx"]], wind_dir_ts=wind_dir_ts[case_lists[c]["wind_case_idx"]], 
-                                                case_name=case_lists[c]["case_names"],
-                                                lut_path=case_lists[c]["lut_path"], generate_lut=case_lists[c]["generate_lut"], seed=case_lists[c]["seed"], wind_field_config=wind_field_config, verbose=False)
-                        for c, d in enumerate(input_dicts)]
-            # dask_wait(futures)
-            results = client.gather(futures)
-            # results = [fut.result() for fut in futures]
-        # elif platform == "darwin":
-        else:
-            if multi == "mpi":
-                comm_size = MPI.COMM_WORLD.Get_size()
-                # comm_rank = MPI.COMM_WORLD.Get_rank()
-                # node_name = MPI.Get_processor_name()
-                executor = MPICommExecutor(MPI.COMM_WORLD, root=0)
-            else:
-                executor = ProcessPoolExecutor()
-            with executor as run_simulations_exec:
-                if multi == "mpi":
-                    run_simulations_exec.max_workers = comm_size
-                print(f"run_simulations line 618 with {run_simulations_exec._max_workers} workers")
-                # for MPIPool executor, (waiting as if shutdown() were called with wait set to True)
-                futures = [run_simulations_exec.submit(simulate_controller, 
-                                                controller_class=globals()[case_lists[c]["controller_class"]], input_dict=d, 
-                                                wind_case_idx=case_lists[c]["wind_case_idx"], wind_mag_ts=wind_mag_ts[case_lists[c]["wind_case_idx"]], wind_dir_ts=wind_dir_ts[case_lists[c]["wind_case_idx"]], 
-                                                case_name=case_lists[c]["case_names"],
-                                                lut_path=case_lists[c]["lut_path"], generate_lut=case_lists[c]["generate_lut"], seed=case_lists[c]["seed"], wind_field_config=wind_field_config, verbose=False)
-                        for c, d in enumerate(input_dicts)]
-                # cf_wait(futures)
-                results = [fut.result() for fut in futures]
+    return case_lists, case_name_lists, input_dicts, wind_field_config, wind_mag_ts, wind_dir_ts
 
-        print("run_simulations line 626")
-
-    else:
-        results = []
-        for c, d in enumerate(input_dicts):
-            results.append(simulate_controller(controller_class=globals()[case_lists[c]["controller_class"]], input_dict=d, 
-                                               wind_case_idx=case_lists[c]["wind_case_idx"], wind_mag_ts=wind_mag_ts[case_lists[c]["wind_case_idx"]], wind_dir_ts=wind_dir_ts[case_lists[c]["wind_case_idx"]], 
-                                               case_name=case_lists[c]["case_names"],
-                                               lut_path=case_lists[c]["lut_path"], generate_lut=case_lists[c]["generate_lut"], seed=case_lists[c]["seed"],
-                                               wind_field_config=wind_field_config, verbose=False))
-
+def save_simulations(case_lists, case_name_lists, results): 
     # unpack rsults for each case
     results_dfs = {}
-    
     for r, res in enumerate(results):
         results_dfs[case_name_lists[r]] = res
 
@@ -868,55 +822,107 @@ def plot_simulations(results_dirs):
     summary_df = pd.read_csv(os.path.join(os.path.dirname(whoc_file), "case_studies", f"comparison_time_series_results.csv"), index_col=0)
     barplot_opt_cost(summary_df, os.path.join(os.path.dirname(whoc_file), "case_studies"), relative=True)
 
-if __name__ == '__main__':
 
-    REGENERATE_WIND_FIELD = False
-    RUN_SIMULATIONS = True
-    POST_PROCESS = True
+REGENERATE_WIND_FIELD = False
+RUN_SIMULATIONS = True
+POST_PROCESS = True
 
-    case_families = ["baseline_controllers", "solver_type",
-                     "wind_preview_type", "warm_start", 
-                     "horizon_length", "breakdown_robustness",
-                     "scalability", "cost_func_tuning", 
-                     "stochastic_preview_type"]
+case_families = ["baseline_controllers", "solver_type",
+                    "wind_preview_type", "warm_start", 
+                    "horizon_length", "breakdown_robustness",
+                    "scalability", "cost_func_tuning", 
+                    "stochastic_preview_type"]
 
-    DEBUG = sys.argv[1].lower() == "debug"
-    # if sys.argv[2].lower() == "dask":
-    #     MULTI = "dask"
-    #     initialize()
-    #     client = Client()
-    if sys.argv[2].lower() == "mpi":
-        MULTI = "mpi"
-    else:
-        MULTI = "cf"
+DEBUG = sys.argv[1].lower() == "debug"
+# if sys.argv[2].lower() == "dask":
+#     MULTI = "dask"
+#     initialize()
+#     client = Client()
+if sys.argv[2].lower() == "mpi":
+    MULTI = "mpi"
+else:
+    MULTI = "cf"
 
-    PARALLEL = sys.argv[3].lower() == "parallel"
-    if len(sys.argv) > 4:
-        CASE_FAMILY_IDX = [int(i) for i in sys.argv[4:]]
-    else:
-        CASE_FAMILY_IDX = list(range(len(case_families)))
+PARALLEL = sys.argv[3].lower() == "parallel"
+if len(sys.argv) > 4:
+    CASE_FAMILY_IDX = [int(i) for i in sys.argv[4:]]
+else:
+    CASE_FAMILY_IDX = list(range(len(case_families)))
 
-    if DEBUG:
-        N_SEEDS = 1
-    else:
-        N_SEEDS = 6
+if DEBUG:
+    N_SEEDS = 1
+else:
+    N_SEEDS = 6
 
-    for case_family in case_families:
-        case_studies[case_family]["wind_case_idx"] = {"group": 2, "vals": [i for i in range(N_SEEDS)]}
+for case_family in case_families:
+    case_studies[case_family]["wind_case_idx"] = {"group": 2, "vals": [i for i in range(N_SEEDS)]}
 
-    # MISHA QUESTION how to make AMR-Wind wait for control solution?
-    # run_simulations(["baseline_controllers"], REGENERATE_WIND_FIELD)
-    # mp.set_start_method('fork')
-    os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "true"
-    # run_simulations(["perfect_preview_type"], REGENERATE_WIND_FIELD)
-    print([case_families[i] for i in CASE_FAMILY_IDX])
-    if RUN_SIMULATIONS:
-        run_simulations([case_families[i] for i in CASE_FAMILY_IDX], regenerate_wind_field=REGENERATE_WIND_FIELD, n_seeds=N_SEEDS, run_parallel=PARALLEL, multi=MULTI)
-
-    if POST_PROCESS:
-        results_dirs = [os.path.join(os.path.dirname(whoc_file), "case_studies", case_families[i]) for i in CASE_FAMILY_IDX]
+# MISHA QUESTION how to make AMR-Wind wait for control solution?
+# run_simulations(["baseline_controllers"], REGENERATE_WIND_FIELD)
+# mp.set_start_method('fork')
+os.environ["PYOPTSPARSE_REQUIRE_MPI"] = "true"
+# run_simulations(["perfect_preview_type"], REGENERATE_WIND_FIELD)
+print([case_families[i] for i in CASE_FAMILY_IDX])
+if RUN_SIMULATIONS:
+    case_lists, case_name_lists, input_dicts, wind_field_config, wind_mag_ts, wind_dir_ts = initialize_simulations([case_families[i] for i in CASE_FAMILY_IDX], regenerate_wind_field=REGENERATE_WIND_FIELD, n_seeds=N_SEEDS, run_parallel=PARALLEL, multi=MULTI)
+    
+    # run simulations
+    print(f"about to submit calls to simulate_controller")
+    run_parallel=PARALLEL
+    multi = MULTI
+    if run_parallel:
+        # if platform == "linux":
         
-        # compute stats over all seeds
-        process_simulations(results_dirs)
-        
-        plot_simulations(results_dirs[0:2])
+        # if multi == "dask":
+        #     futures = [client.submit(simulate_controller, 
+        #                                         controller_class=globals()[case_lists[c]["controller_class"]], input_dict=d, 
+        #                                         wind_case_idx=case_lists[c]["wind_case_idx"], wind_mag_ts=wind_mag_ts[case_lists[c]["wind_case_idx"]], wind_dir_ts=wind_dir_ts[case_lists[c]["wind_case_idx"]], 
+        #                                         case_name=case_lists[c]["case_names"],
+        #                                         lut_path=case_lists[c]["lut_path"], generate_lut=case_lists[c]["generate_lut"], seed=case_lists[c]["seed"], wind_field_config=wind_field_config, verbose=False)
+        #                 for c, d in enumerate(input_dicts)]
+        #     # dask_wait(futures)
+        #     results = client.gather(futures)
+        #     # results = [fut.result() for fut in futures]
+        # # elif platform == "darwin":
+        # else:
+        if multi == "mpi":
+            comm_size = MPI.COMM_WORLD.Get_size()
+            # comm_rank = MPI.COMM_WORLD.Get_rank()
+            # node_name = MPI.Get_processor_name()
+            executor = MPICommExecutor(MPI.COMM_WORLD, root=0)
+        else:
+            executor = ProcessPoolExecutor()
+        with executor as run_simulations_exec:
+            if multi == "mpi":
+                run_simulations_exec.max_workers = comm_size
+            print(f"run_simulations line 618 with {run_simulations_exec._max_workers} workers")
+            # for MPIPool executor, (waiting as if shutdown() were called with wait set to True)
+            futures = [run_simulations_exec.submit(simulate_controller, 
+                                            controller_class=globals()[case_lists[c]["controller_class"]], input_dict=d, 
+                                            wind_case_idx=case_lists[c]["wind_case_idx"], wind_mag_ts=wind_mag_ts[case_lists[c]["wind_case_idx"]], wind_dir_ts=wind_dir_ts[case_lists[c]["wind_case_idx"]], 
+                                            case_name=case_lists[c]["case_names"],
+                                            lut_path=case_lists[c]["lut_path"], generate_lut=case_lists[c]["generate_lut"], seed=case_lists[c]["seed"], wind_field_config=wind_field_config, verbose=False)
+                    for c, d in enumerate(input_dicts)]
+            # cf_wait(futures)
+            results = [fut.result() for fut in futures]
+
+        print("run_simulations line 626")
+
+    else:
+        results = []
+        for c, d in enumerate(input_dicts):
+            results.append(simulate_controller(controller_class=globals()[case_lists[c]["controller_class"]], input_dict=d, 
+                                               wind_case_idx=case_lists[c]["wind_case_idx"], wind_mag_ts=wind_mag_ts[case_lists[c]["wind_case_idx"]], wind_dir_ts=wind_dir_ts[case_lists[c]["wind_case_idx"]], 
+                                               case_name=case_lists[c]["case_names"],
+                                               lut_path=case_lists[c]["lut_path"], generate_lut=case_lists[c]["generate_lut"], seed=case_lists[c]["seed"],
+                                               wind_field_config=wind_field_config, verbose=False))
+    
+    save_simulations(case_lists, case_name_lists, results)
+            
+if POST_PROCESS:
+    results_dirs = [os.path.join(os.path.dirname(whoc_file), "case_studies", case_families[i]) for i in CASE_FAMILY_IDX]
+    
+    # compute stats over all seeds
+    process_simulations(results_dirs)
+    
+    plot_simulations(results_dirs[0:2])
