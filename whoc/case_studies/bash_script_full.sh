7e181718368140562e6db8def303bc76abb9367d
Aoife Henry <ahenry@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-05-07 16:31:59 -0600

    kestrel updates

--- whoc/case_studies/bash_script_full.sh
+++ whoc/case_studies/bash_script_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=full_floris_case_studies.py
-#SBATCH --time=48:00:00
-#SBATCH --nodes=2
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
 #SBATCH --ntasks=104
 #SBATCH --account=ssc
 
@@ -11,11 +11,6 @@ ml mamba
 conda activate whoc
 #env MPICC=cc pip install dask-mpi mpi4py
 
-
-#python run_case_studies.py nodebug nompi parallel 0 1 2 3 4 5 6 7
-# srun python run_case_studies.py debug nompi 0 1 2 3 4 5 6 7
 rm -rf /projects/ssc/ahenry/whoc/floris_case_studies
 export MAXWORKERS=`echo $(($SLURM_CPUS_ON_NODE * $SLURM_JOB_NUM_NODES))`
 srun -n $MAXWORKERS python run_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
-#srun -n $SLURM_NTASKS python run_case_studies.py nodebug dask parallel 0 1 2 3 4 5 6 7
-# srun python run_case_studies.py
