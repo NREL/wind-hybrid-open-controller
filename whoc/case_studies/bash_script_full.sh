8713d8814dc9964d39f313d61fe7346c0f928fb8
Aoife Henry <ahenry@kl1.head.cm.kestrel.hpc.nrel.gov>
2024-05-07 12:36:58 -0600

    kestrel updates

--- whoc/case_studies/bash_script_full.sh
+++ whoc/case_studies/bash_script_full.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=full_floris_case_studies.py
 #SBATCH --time=48:00:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks=104
 #SBATCH --account=ssc
 
 module purge
-#module load cray-python 
-module load anaconda3
-#module load openmpi
-#module load conda
+ml restore system
+ml mamba
 conda activate whoc
-module load PrgEnv-cray
-env MPICC=cc pip install dask-mpi mpi4py
+#env MPICC=cc pip install dask-mpi mpi4py
 
 
 #python run_case_studies.py nodebug nompi parallel 0 1 2 3 4 5 6 7
 # srun python run_case_studies.py debug nompi 0 1 2 3 4 5 6 7
 rm -rf /projects/ssc/ahenry/whoc/floris_case_studies
-srun -n $SLURM_NTASKS python run_case_studies.py nodebug dask parallel 0 1 2 3 4 5 6 7
+export MAXWORKERS=`echo $(($SLURM_CPUS_ON_NODE * $SLURM_JOB_NUM_NODES))`
+srun -n $MAXWORKERS python run_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
 #srun -n $SLURM_NTASKS python run_case_studies.py nodebug dask parallel 0 1 2 3 4 5 6 7
 # srun python run_case_studies.py
