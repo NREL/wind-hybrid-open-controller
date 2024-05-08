1be8fd4afe0dfb31333ffdb569e41c95f0a8f399
Aoife Henry <ahenry@kl1.head.cm.kestrel.hpc.nrel.gov>
2024-05-08 09:50:57 -0600

    kestrel cleanup

--- whoc/case_studies/bash_script_full.sh
+++ whoc/case_studies/bash_script_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=full_floris_case_studies.py
-#SBATCH --time=01:00:00
-#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --nodes=2
 #SBATCH --ntasks=104
 #SBATCH --account=ssc
 
@@ -13,4 +13,6 @@ conda activate whoc
 
 rm -rf /projects/ssc/ahenry/whoc/floris_case_studies
 export MAXWORKERS=`echo $(($SLURM_CPUS_ON_NODE * $SLURM_JOB_NUM_NODES))`
-srun -n $MAXWORKERS python run_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
+python initialize_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
+srun -n $MAXWORKERS python simulate_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
+python process_case_studies.py nodebug mpi parallel 0 1 2 3 4 5 6 7
