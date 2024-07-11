a710d464d51f0cac6868c129cbace2c780c1b61f
Aoife Henry <aoife.henry@colorado.edu>
2024-07-11 11:16:05 -0600

    tuned mpc

--- whoc/wind_field/transfer_wind_field_data_from_kestrel.sh
+++ whoc/wind_field/transfer_wind_field_data_from_kestrel.sh
@@ -1,3 +1,3 @@
 scp -r ahenry@kestrel.hpc.nrel.gov:/home/ahenry/toolboxes/whoc_env/wind-hybrid-open-controller/examples/post_processing /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples
 
-scp -r ahenry@kestrel.hpc.nrel.gov:/home/ahenry/toolboxes/whoc_env/wind-hybrid-open-controller/examples/floris_case_studies /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples
+scp -r ahenry@kestrel.hpc.nrel.gov:/projects/ssc/ahenry/whoc/floris_case_studies /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples
