a74c21fa0901d4adf3d80c6429f4ecb79e84ab6e
Aoife Henry <aoife.henry@colorado.edu>
2024-07-16 15:57:14 -0600

    updates

--- whoc/case_studies/initialize_case_studies.py
+++ whoc/case_studies/initialize_case_studies.py
@@ -162,28 +162,25 @@ case_studies = {
                           },
     "breakdown_robustness": 
         {"seed": {"group": 0, "vals": [0]},
-         "num_turbines": {"group": 0, "vals": [25]}, 
-         "controller_class": {"group": 0, "vals": ["MPC"]},
+         "controller_class": {"group": 1, "vals": ["MPC", "LookupBasedWakeSteeringController", "GreedyController"]},
+         "dt": {"group": 1, "vals": [60, 5, 5]},
+         "use_filtered_wind_dir": {"group": 1, "vals": [False, True, True]},
          "lut_path": {"group": 0, "vals": [os.path.join(os.path.dirname(whoc_file), 
                                                                         f"../examples/mpc_wake_steering_florisstandin/lookup_tables/lut_{25}.csv")]},
           "floris_input_file": {"group": 0, "vals": [os.path.join(os.path.dirname(whoc_file), 
                                                                         f"../examples/mpc_wake_steering_florisstandin/floris_gch_{25}.yaml")]},
-          "case_names": {"group": 1, "vals": [f"{f*100:04.1f}% Chance of Breakdown" for f in list(np.linspace(0, 0.5, N_COST_FUNC_TUNINGS))]},
-          "offline_probability": {"group": 1, "vals": list(np.linspace(0, 0.5, N_COST_FUNC_TUNINGS))}
+        #   "case_names": {"group": 1, "vals": [f"{f*100:04.1f}% Chance of Breakdown" for f in list(np.linspace(0, 0.5, N_COST_FUNC_TUNINGS))]},
+          "offline_probability": {"group": 2, "vals": list(np.linspace(0, 0.5, N_COST_FUNC_TUNINGS))}
         },
     "scalability": {"seed": {"group": 0, "vals": [0]},
-                    "controller_class": {"group": 0, "vals": ["MPC"]},
-                    "case_names": {"group": 1, "vals": ["3 Turbines", "9 Turbines", "25 Turbines", "100 Turbines"]},
-                    # "case_names": {"group": 1, "vals": ["9 Turbines", "25 Turbines", "100 Turbines"]},
-                    "generate_lut": {"group": 0, "vals": [True]},
-                    "lut_path": {"group": 1, "vals": [os.path.join(os.path.dirname(whoc_file), 
-                                                                        f"../examples/mpc_wake_steering_florisstandin/lookup_tables/lut_{nturb}.csv") for nturb in [3, 9, 25, 100]]},
-                    # "lut_path": {"group": 1, "vals": [os.path.join(os.path.dirname(whoc_file), 
-                    #                                                     f"../examples/mpc_wake_steering_florisstandin/lookup_tables/lut_{nturb}.csv") for nturb in [9, 25, 100]]},
-                    "floris_input_file": {"group": 1, "vals": [os.path.join(os.path.dirname(whoc_file), "../examples/mpc_wake_steering_florisstandin", 
-                                                             f"floris_gch_{i}.yaml") for i in [3, 9, 25, 100]]
-                    # "floris_input_file": {"group": 1, "vals": [os.path.join(os.path.dirname(whoc_file), "../examples/mpc_wake_steering_florisstandin", 
-                    #                                          f"floris_gch_{i}.yaml") for i in [3, 9, 25, 100]]
+                    "controller_class": {"group": 1, "vals": ["MPC", "LookupBasedWakeSteeringController", "GreedyController"]},
+                    "dt": {"group": 1, "vals": [60, 5, 5]},
+                    "use_filtered_wind_dir": {"group": 1, "vals": [False, True, True]},
+                    # "case_names": {"group": 2, "vals": ["3 Turbines", "9 Turbines", "25 Turbines"]},
+                    "lut_path": {"group": 2, "vals": [os.path.join(os.path.dirname(whoc_file), 
+                                                                        f"../examples/mpc_wake_steering_florisstandin/lookup_tables/lut_{nturb}.csv") for nturb in [3, 9, 25]]},
+                    "floris_input_file": {"group": 2, "vals": [os.path.join(os.path.dirname(whoc_file), "../examples/mpc_wake_steering_florisstandin", 
+                                                             f"floris_gch_{i}.yaml") for i in [3, 9, 25]]
                                                              
                                                              },
                                                              
