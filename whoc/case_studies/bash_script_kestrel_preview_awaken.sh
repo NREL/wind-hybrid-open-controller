9b697d7bca2c030dc5ec3ce114bb5fe786fe8fd0
Aoife Henry <aoife.henry@colorado.edu>
2025-02-05 09:53:58 -0700

    up

--- whoc/case_studies/bash_script_kestrel_preview_awaken.sh
+++ whoc/case_studies/bash_script_kestrel_preview_awaken.sh
@@ -14,7 +14,5 @@ export LD_LIBRARY_PATH=/home/ahenry/.conda-envs/whoc/lib
 echo $SLURM_NTASKS
 mpirun -np $SLURM_NTASKS python run_case_studies.py 15 -rs -rrs -st 3600 -ns 3 -p -m mpi \
         -sd /projects/ssc/ahenry/whoc/floris_case_studies \
-        -mcnf wind_forecasting_env/wind-forecasting/examples/inputs/training_inputs_kestrel_flasc.yaml \
+        -mcnf wind_forecasting_env/wind-forecasting/examples/inputs/training_inputs_kestrel_awaken.yaml \
         -wf scada
-#mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 7 8 9 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
-#mpirun -np $SLURM_NTASKS python run_case_studies.py 5 6 -p -rs -st 3600 -ns 6 -m mpi -sd /projects/ssc/ahenry/whoc/floris_case_studies
