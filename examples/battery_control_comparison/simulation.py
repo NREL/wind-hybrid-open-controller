60df766ebe68ee3d113283b3d90446ddb564cf2c
misi9170 <michael.sinner@nrel.gov>
2025-03-18 13:27:17 -0600

    Simulate various gains.

--- examples/battery_control_comparison/simulation.py
+++ examples/battery_control_comparison/simulation.py
@@ -16,78 +16,100 @@ input_dict = {
             "discharge_rate": 20,
             "max_SOC": 0.9,
             "min_SOC": 0.1,
-            "initial_conditions": {"SOC": 0.8}
+            "initial_conditions": {"SOC": 0.5}
         }
     },
     "controller": {
-        "k_p_max": 1.0,#0.1,#1.0,
-        "k_p_min": 0.01,#0.1,
+        "k_p_max": 1.0, # These will be overwritten during simulation
+        "k_p_min": 1.0, # These will be overwritten during simulation
     }
 }
 
-runtime_dict = {
-    "time": 0,
-    "external_signals": {"plant_power_reference": 0},
-    "py_sims": {
-        "battery": {"outputs": {"power": 0, "soc": 0, "reject": 0}},
-        "inputs": {"battery_signal": 0, "available_power": 20000}
-    }
-}
-
-# Establish simulation components
-battery=Battery(input_dict["py_sims"]["battery"], dt)
-interface=HerculesBatteryInterface(input_dict)
-controller=BatteryController(interface, input_dict)
-
-# Create inputs
-seg_len = round(100*dt)
+# Make reference input sequence
+np.random.seed(0)
+hour = round(3600/dt)
 reference_input_sequence = np.concatenate((
-    10000*np.ones(seg_len),
-    5000*np.ones(seg_len),
-    10000*np.ones(seg_len),
-    0*np.ones(seg_len),
-    -10000*np.ones(seg_len),
-    -20000*np.ones(seg_len),
-    -30000*np.ones(seg_len),
+    20000*np.ones(2*hour),
+    -20000*np.ones(3*hour),
+    10000*np.ones(1*hour),
+    0*np.ones(1*hour),
+    -10000*np.ones(1*hour),
+    0*np.ones(1*hour),
+    np.repeat(0+5000*np.random.randn(round(hour/(12*60))), (12*60)),
 ))
-time = np.arange(0, len(reference_input_sequence)*dt, dt)
-power_sequence = np.zeros_like(reference_input_sequence)
-soc_sequence = np.zeros_like(reference_input_sequence)
-battery_signal_sequence = np.zeros_like(reference_input_sequence)
 
 
-# Run the simulation
-for i, (t, r) in enumerate(zip(time, reference_input_sequence)):
-    # Update time and reference signals
-    runtime_dict["time"] = t
-    runtime_dict["external_signals"]["plant_power_reference"] = r
+# Create some functions for simulating for simplicity
+def simulate(k_p_max, k_p_min):
+    input_dict["controller"]["k_p_max"] = k_p_max
+    input_dict["controller"]["k_p_min"] = k_p_min
 
-    # Run controller
-    runtime_dict = controller.step(runtime_dict)
 
-    # Sign switch since battery assumes positive power is charging
-    battery_signal_sequence[i] = runtime_dict["py_sims"]["inputs"]["battery_signal"]
-    runtime_dict["py_sims"]["inputs"]["battery_signal"] *= -1
+    # Establish simulation components
+    battery=Battery(input_dict["py_sims"]["battery"], dt)
+    interface=HerculesBatteryInterface(input_dict)
+    controller=BatteryController(interface, input_dict)
 
-    # Run battery simulator
-    runtime_dict["py_sims"]["battery"]["outputs"] = battery.step(runtime_dict)
 
-    # Save outputs (switch sign back)
-    power_sequence[i] = -runtime_dict["py_sims"]["battery"]["outputs"]["power"]
-    soc_sequence[i] = runtime_dict["py_sims"]["battery"]["outputs"]["soc"]
+    runtime_dict = {
+        "time": 0,
+        "external_signals": {"plant_power_reference": 0},
+        "py_sims": {
+            "battery": {"outputs": {"power": 0, "soc": 0, "reject": 0}},
+            "inputs": {"battery_signal": 0, "available_power": 20000}
+        }
+    }
 
-# Plot
-fig, ax = plt.subplots(2, 1, sharex=True)
-ax[0].plot(time, power_sequence, color="C0", label="Battery output")
-ax[0].plot(time, reference_input_sequence, color="black", linestyle="dashed", label="Reference")
-ax[0].plot(time, battery_signal_sequence, color="red", linestyle="dashed", label="Battery signal")
-ax[0].set_ylabel("Power [kW]")
-ax[0].legend()
-ax[1].plot(time, soc_sequence, color="C0", label="SOC")
-ax[1].set_ylabel("SOC [-]")
-ax[1].set_xlabel("Time [s]")
-ax[1].set_xlim([time[0], time[-1]])
-ax[0].grid()
-ax[1].grid()
+    # Create inputs
+    time = np.arange(0, len(reference_input_sequence)*dt, dt)
+    power_sequence = np.zeros_like(reference_input_sequence)
+    soc_sequence = np.zeros_like(reference_input_sequence)
+
+    # Simulation loop
+    for i, (t, r) in enumerate(zip(time, reference_input_sequence)):
+        # Update time and reference signals
+        runtime_dict["time"] = t
+        runtime_dict["external_signals"]["plant_power_reference"] = r
+
+        # Run controller
+        runtime_dict = controller.step(runtime_dict)
+
+        # Sign switch since battery assumes positive power is charging
+        runtime_dict["py_sims"]["inputs"]["battery_signal"] *= -1
+
+        # Run battery simulator
+        runtime_dict["py_sims"]["battery"]["outputs"] = battery.step(runtime_dict)
+
+        # Save outputs (switch sign back)
+        power_sequence[i] = -runtime_dict["py_sims"]["battery"]["outputs"]["power"]
+        soc_sequence[i] = runtime_dict["py_sims"]["battery"]["outputs"]["soc"]
+
+    return time, power_sequence, soc_sequence
+
+def plot_results(time, power_sequence, soc_sequence):
+    # Plot
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    ax[0].plot(time, power_sequence, color="C0", label="Battery output")
+    ax[0].plot(time, reference_input_sequence, color="black", linestyle="dashed", label="Reference")
+    ax[0].set_ylabel("Power [kW]")
+    ax[0].legend()
+    ax[1].plot(time, soc_sequence, color="C0", label="SOC")
+    ax[1].set_ylabel("SOC [-]")
+    ax[1].set_xlabel("Time [h]")
+    ax[1].set_xlim([time[0], time[-1]])
+    ax[0].grid()
+    ax[1].grid()
+    ax[0].set_title("k_p_max = {:.3f}, k_p_min = {:.3f}".format(k_p_max, k_p_min))
+    ax[0].plot([time[0], time[-1]], [20000, 20000], color="red", linestyle="dotted")
+    ax[0].plot([time[0], time[-1]], [-20000, -20000], color="red", linestyle="dotted")
+
+# Loop over different gains
+k_p_max_vals = [1.0, 0.01, 1.0]
+k_p_min_vals = [1.0, 0.01, 0.01]
+
+# Run simulations and plot
+for k_p_max, k_p_min in zip(k_p_max_vals, k_p_min_vals):
+    time, power_sequence, soc_sequence = simulate(k_p_max, k_p_min)
+    plot_results(time/3600, power_sequence, soc_sequence)
 
 plt.show()
