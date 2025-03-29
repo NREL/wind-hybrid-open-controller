37309bc9f9f87f408e2f9a396e32ba89e73d392a
Aoife Henry <aoife.henry@colorado.edu>
2025-03-29 17:51:56 -0600

    upd

--- whoc/wind_forecast/WindForecast.py
+++ whoc/wind_forecast/WindForecast.py
@@ -140,8 +140,8 @@ class WindForecast:
             # evaluate with cross-validation
             logging.info(f"Computing score for output {output}.")
             # use joblib.parallel_backend spawn to allow multiprocessing here
-            with parallel_backend('threading', inner_max_num_threads=2):
-                total_score += cross_val_score(model, X_train, y_train, n_jobs=None, cv=2, scoring="neg_mean_squared_error").mean()
+            # with parallel_backend('threading', inner_max_num_threads=2):
+            total_score += cross_val_score(model, X_train, y_train, n_jobs=None, cv=2, scoring="neg_mean_squared_error").mean()
             
             # total_score += (-mean_squared_error(y_true, y_pred))
         
