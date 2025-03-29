96ff3384d7c47c7c309a263682a05b1468ac4082
Aoife Henry <aoife.henry@colorado.edu>
2025-03-29 11:44:33 -0600

    updated bash script

--- whoc/case_studies/bash_script_rc.sh
+++ whoc/case_studies/bash_script_rc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=full_floris_case_studies.py
-#SBATCH --mem=0
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=64
-#SBATCH --time=24:00:00
-#SBATCH --partition=amilan
-##SBATCH --time=01:00:00
-##SBATCH --partition=debug
+##SBATCH --mem=0
+##SBATCH --nodes=4
+##SBATCH --ntasks-per-node=64
+##SBATCH --time=24:00:00
+##SBATCH --partition=amilan
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=01:00:00
+#SBATCH --partition=atesting
 
 # load modules
 module purge
 module load miniforge 
-mamba activate whoc
-module load intel/2022.1.2 impi/2021.5.0
+conda activate whoc
+module load intel impi
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/projects/aohe7145/software/anaconda/envs/whoc/lib
 
-#conda activate whoc
 echo $SLURM_NTASKS
-#export SLURM_EXPORT_ENV=ALL
-#python run_case_studies.py debug cf parallel 0 1 2 3 4 5 6 7
-mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/aohe7145/toolboxes/wind-hybrid-open-controller/whoc/floris_case_studies
+mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 -rs -st 120 -ns 1 -p -m mpi -sd /projects/aohe7145/toolboxes/whoc_env/wind-hybrid-open-controller/examples/floris_case_studies -wcnf /projects/aohe7145/toolboxes/whoc_env/wind-hybrid-open-controller/examples/hercules_input_001.yaml -wf floris
+#mpirun -np $SLURM_NTASKS python run_case_studies.py 0 1 2 3 4 5 6 -rs -st 3600 -ns 6 -p -m mpi -sd /projects/aohe7145/toolboxes/wind-hybrid-open-controller/whoc/floris_case_studies
