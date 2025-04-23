d88ee463b763a6475de3d7f98d4bcda0f28b057f
misi9170 <michael.sinner@nrel.gov>
2025-04-23 16:20:36 -0600

    Add interface for Hercules in LongSim mode.

--- whoc/interfaces/__init__.py
+++ whoc/interfaces/__init__.py
@@ -1,4 +1,7 @@
-from whoc.interfaces.hercules_actuator_disk_interface import HerculesADInterface
+from whoc.interfaces.hercules_actuator_disk_interface import (
+    HerculesADInterface,
+    HerculesLSADInterface,
+)
 from whoc.interfaces.hercules_battery_interface import HerculesBatteryInterface
 from whoc.interfaces.hercules_hybrid_actuator_disk_interface import HerculesHybridADInterface
 from whoc.interfaces.rosco_zmq_interface import ROSCO_ZMQInterface
