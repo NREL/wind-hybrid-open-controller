d1ce624856d56ecc32a3feb7c6fe96021c88a8a2
misi9170 <michael.sinner@nrel.gov>
2025-06-07 11:59:59 -0600

    Simplify output plots

--- examples/wind_hydrogen_tracking/plot_output_data.py
+++ examples/wind_hydrogen_tracking/plot_output_data.py
@@ -7,66 +7,52 @@ h2_ref_input = pd.read_csv("inputs/hydrogen_ref_signal.csv")
 
 # Extract individual components powers as well as total power
 n_wind_turbines = 9
-wind_power = df[["hercules_comms.amr_wind.wind_farm_0.turbine_powers.{0:03d}".format(t)
-                 for t in range(n_wind_turbines)]].to_numpy().sum(axis=1) / 1e3
 hydrogen_output_rate = df["py_sims.hydrogen_plant_0.outputs.H2_mfr"]
-# power_output = (df["py_sims.inputs.available_power"]) / 1e3
-time = df["hercules_comms.amr_wind.wind_farm_0.sim_time_s_amr_wind"] / 60 # minutes
+time = df["hercules_comms.amr_wind.wind_farm_0.sim_time_s_amr_wind"]
 
 # Set plotting aesthetics
 wind_col = "C0"
-h2_col = "C2"
-plant_col = "C3"
-
-
-wind_power_individuals = df[["hercules_comms.amr_wind.wind_farm_0.turbine_powers.{0:03d}".format(t)
-                             for t in range(n_wind_turbines)]].to_numpy() / 1e3
+h2_col = "k"
+power_err_col = "red"
+h2_ref_col = "red"
 
 # Plot the hydrogen output 
-fig, ax = plt.subplots(3, 1, sharex=True, figsize=(12,8))
-ax[0].plot(time, wind_power, color=plant_col, label="Total Plant Power")
-ax[0].plot(time, df["py_sims.inputs.locally_generated_power"], label="Generated power")
-ax[0].plot(time, df["py_sims.inputs.plant_outputs.electricity"], label="Plant output")
-ax[0].plot(time, df["py_sims.hydrogen_plant_0.outputs.power_used_kw"], \
-        label="Electrolyzer power used")
+fig, ax = plt.subplots(2, 1, sharex=True, figsize=(12,8))
+ax[0].plot(
+    time/60,
+    df["py_sims.inputs.locally_generated_power"]/1e3,
+    color=wind_col,
+    label="Power generated"
+)
+ax[0].plot(
+    time/60,
+    df["py_sims.hydrogen_plant_0.outputs.power_used_kw"]/1e3,
+    color=h2_col,
+    label="Electrolyzer power consumed",
+    linestyle="--"
+)
+ax[0].fill_between(
+    time/60,
+    df["py_sims.inputs.plant_outputs.electricity"]/1e3,
+    color=power_err_col,
+    label="Generation/consumption mismatch"
+)
 ax[0].set_ylabel("Power [MW]")
 ax[0].grid()
-ax[0].legend()
+ax[0].legend(loc="lower right")
 
-turbines = range(0,9)
-width = 12
-alpha = 0.25
-red_col = 0.5
-blue_col = 1.0
-for i in turbines:
-    turb_num = i
-    turb_str = str(turb_num)
-    ax[1].plot(time,  wind_power_individuals[:,i], label="WT"+str(i), \
-               color=(0.5, blue_col, red_col), linewidth=width, alpha = alpha)
-    width = width - 1.25
-    alpha = alpha+0.075
-    red_col = red_col+0.05
-    blue_col = blue_col-0.05
-# for i in range (n_wind_turbines):
-#     ax[1].plot(time, wind_power_individuals[:,i], label="WT"+str(i), alpha=0.7, color=wind_col)
+ax[1].plot(
+    h2_ref_input["time"]/60,
+    h2_ref_input["hydrogen_reference"],
+    color=h2_ref_col,
+    label="Hydrogen reference",
+    linestyle=":"
+)
+ax[1].plot(time/60, hydrogen_output_rate, color=h2_col, label='Hydrogen output rate')
+ax[1].set_ylabel("Hydrogen Production Rate [kg/s]")
 ax[1].grid()
-ax[1].legend()
-
-ax[2].plot(h2_ref_input['time'] / 60, h2_ref_input['hydrogen_reference'],\
-            'k--', label="Hydrogen Reference")
-ax[2].plot(time, hydrogen_output_rate, color=h2_col, label='Hydrogen Rate Output')
-ax[2].set_ylabel("Hydrogen Production Rate [kg/s]")
-ax[2].grid()
-ax[2].legend()
-ax[2].set_xlabel("Time [mins]")
+ax[1].legend(loc="lower right")
+ax[1].set_xlabel("Time [mins]")
 
 # fig.savefig("../../docs/graphics/wind-hydrogen-example-plot.png", dpi=300, format="png")
-
-fig1, ax1 = plt.subplots(1, 1, sharex=True, figsize=(12,8))
-ax1.plot(time, hydrogen_output_rate, color=plant_col, label="H2 mass flow rate")
-# ax1.plot(time, hydrogen_output, color=h2_col, label="H2 produced")
-ax1.set_ylabel("Power [MW]")
-ax1.grid()
-ax1.legend()
-
 plt.show()
