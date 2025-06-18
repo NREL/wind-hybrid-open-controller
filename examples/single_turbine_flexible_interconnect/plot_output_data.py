f6c6add1542a92aadd9be7a26d7ed269aaa0d53d
misi9170 <michael.sinner@nrel.gov>
2025-06-18 13:08:16 -0600

    More realistic wind speed input

--- examples/single_turbine_flexible_interconnect/plot_output_data.py
+++ examples/single_turbine_flexible_interconnect/plot_output_data.py
@@ -10,8 +10,10 @@ wd_cols = [wf_str+"turbine_wind_directions.{0:03d}".format(t) for t in range(n_t
 yaw_cols = [wf_str+"turbine_yaw_angles.{0:03d}".format(t) for t in range(n_turbines)]
 ref_col = "external_signals.wind_power_reference"
 
+ws_col = "hercules_comms.amr_wind.wind_farm_0.wind_speed"
+
 # Create plots
-fig, ax = plt.subplots(1, 1, sharex=True, sharey=True)
+fig, ax = plt.subplots(2, 1, sharex=True)
 fig.set_size_inches(10, 5)
 
 # Extract data from larger array
@@ -21,23 +23,27 @@ wds = df[wd_cols].to_numpy()
 yaws = df[yaw_cols].to_numpy()
 ref = df[ref_col].to_numpy()
 
-# Direction
+ax[0].plot(time/3600, df[ws_col], color="C0", label="Wind speed")
+ax[0].set_ylabel("Wind speed [m/s]")
+ax[0].grid()
+
+# Power output
 for t in range(n_turbines):
     if t == 0:
-        line = ax.fill_between(time/3600, powers[:,t], label="T{0:03d} power".format(t))
+        line = ax[1].fill_between(time/3600, powers[:,t], label="T{0:03d} power".format(t))
     else:
-        ax.fill_between(time/3600, powers[:,:t+1].sum(axis=1), powers[:,:t].sum(axis=1),
+        ax[1].fill_between(time/3600, powers[:,:t+1].sum(axis=1), powers[:,:t].sum(axis=1),
             label="T{0:03d} power".format(t))
-ax.plot(time/3600, powers.sum(axis=1), color="black", label="Farm power")
-ax.plot(time/3600, ref, color="gray", linestyle="dashed", label="Ref. power")
+ax[1].plot(time/3600, powers.sum(axis=1), color="black", label="Farm power")
+ax[1].plot(time/3600, ref, color="gray", linestyle="dashed", label="Ref. power")
 
 # Plot aesthetics
-ax.grid()
-ax.set_ylabel("Power [kW]")
-ax.set_xlim([time[0]/3600, time[-1]/3600])
-ax.set_ylim([0, 2000])
-ax.legend(loc="lower left")
-ax.set_xlabel("Time [hr]")
+ax[1].grid()
+ax[1].set_ylabel("Power [kW]")
+ax[1].set_xlim([time[0]/3600, time[-1]/3600])
+ax[1].set_ylim([0, 2000])
+ax[1].legend(loc="lower left")
+ax[1].set_xlabel("Time [hr]")
 
 # fig.savefig("../../docs/graphics/flexible-interconnect.png", dpi=300, format="png")
 
