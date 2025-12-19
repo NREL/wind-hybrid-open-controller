de543c2d59c982d0b788c591d6199e60dbbd26a0
paulf81 <paul.fleming@nrel.gov>
2025-12-19 09:30:37 -0700

    set high soc default to 1 (#86)
    * set high soc default to 1
    
    * add defaults
    
    * linting
    
    * sp
    
    * Minor docstring cleanup
    
    ---------
    
    Co-authored-by: misi9170 <michael.sinner@nrel.gov>

--- hycon/controllers/battery_controller.py
+++ hycon/controllers/battery_controller.py
@@ -213,22 +213,26 @@ class BatteryPriceSOCController(ControllerBase):
 
     def set_controller_parameters(
         self,
-        high_soc=0.8,
+        high_soc=1.0,
         low_soc=0.2,
         **_,  # <- Allows arbitrary additional parameters to be passed, which are ignored
     ):
         """
         Set parameters for BatteryPriceSOCController.
 
-        high_soc is the SOC threshold above which the battery will only charge if the price is above
-        the highest (hourly) DA price of the day.
+        high_soc is the SOC threshold above which the battery will only charge if the price is below
+        the lowest (hourly) DA price of the day.  Defaults to 1.0.
 
         low_soc is the SOC threshold below which the battery will only discharge if the price is
-        below the lowest (hourly) DA price of the day.
+        above the highest (hourly) DA price of the day.  Defaults to 0.2.
+
+        high_soc defaults to 1.0 (effectively disabled) as experience suggests waiting for
+        very low prices is not worthwhile. low_soc defaults to 0.2 as experience suggests waiting
+        for very high prices is worthwhile.
 
         Args:
-            high_soc (float): High SOC threshold (0 to 1).
-            low_soc (float): Low SOC threshold (0 to 1).
+            high_soc (float): High SOC threshold (0 to 1).  Defaults to 1.0.
+            low_soc (float): Low SOC threshold (0 to 1).  Defaults to 0.2.
         """
         self.high_soc = high_soc
         self.low_soc = low_soc
