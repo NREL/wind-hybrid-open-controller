13ec2fc20f73c8d743b637dd9e7c4e5e6640c95f
misi9170 <michael.sinner@nlr.gov>
2026-06-17 11:52:24 -0600

    rate limiter, saturation, tests for both

--- hycon/controllers/signal_conditioning.py
+++ hycon/controllers/signal_conditioning.py
@@ -43,3 +43,54 @@ class LowPassFilter:
         y, self._x = signal.lfilter(self._bz, self._az, [u], zi=self._x)
 
         return y[0]
+
+
+class RateLimiter:
+    def __init__(self, max_rate_up, max_rate_down=None, dt=1.0):
+        """
+        Instantiates the rate limiter.
+
+        Args:
+            max_rate_up (float): Maximum allowed rate of increase of the signal (units per second).
+            max_rate_down (float): Maximum allowed rate of decrease of the signal (units per
+                second).
+            dt (float): Time step for discretization of the filter.
+        """
+
+        self._max_rate_up = max_rate_up * dt
+        self._max_rate_down = -(max_rate_down or max_rate_up) * dt
+
+        self._x = 0.0
+
+    def __call__(self, u):
+        """
+        Computes the rate-limited output for a given (scalar) input.
+        """
+
+        delta = u - self._x
+        delta_clipped = np.clip(delta, self._max_rate_down, self._max_rate_up)
+        output = self._x + delta_clipped
+        self._x = output
+
+        return output
+
+
+class Saturator:
+    def __init__(self, min_value=None, max_value=None):
+        """
+        Instantiates the saturator.
+
+        Args:
+            min_value (float): Minimum allowed value of the signal.
+            max_value (float): Maximum allowed value of the signal.
+        """
+
+        self._min_value = min_value
+        self._max_value = max_value
+
+    def __call__(self, u):
+        """
+        Computes the saturated output for a given (scalar) input.
+        """
+
+        return np.clip(u, self._min_value, self._max_value)
--- tests/controller_utilities_test.py
+++ tests/controller_utilities_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from hycon.controllers.signal_conditioning import LowPassFilter
+from hycon.controllers.signal_conditioning import LowPassFilter, RateLimiter, Saturator
 
 
 def test_LowPassFilter():
@@ -52,3 +52,53 @@ def test_LowPassFilter():
     for _ in range(100):
         y = lpf(power_setpoint_ref)
     assert np.isclose(y, power_setpoint_ref, atol=1e-2)
+
+def test_RateLimiter():
+    """
+    Tests that the RateLimiter outputs a reasonable signal
+    """
+    dt_test = 2.0
+    max_rate_up = 100
+    max_rate_down = 50
+    rate_limiter = RateLimiter(max_rate_up=max_rate_up, max_rate_down=max_rate_down, dt=dt_test)
+
+    # Test with input
+    rate_limiter._x = 500
+    out = rate_limiter(1000)
+    assert out == 500 + 2*100
+    out = rate_limiter(-1000)
+    assert out == 500 + 2*100 - 2*50
+
+    # Test not specifying max_rate_down (uses max_rate_up for both)
+    rate_limiter = RateLimiter(max_rate_up=max_rate_up, dt=dt_test)
+    rate_limiter._x = 500
+    out = rate_limiter(-1000)
+    assert out == 500 - 2*100
+
+def test_Saturator():
+    """
+    Tests that the Saturator outputs a reasonable signal
+    """
+    saturator = Saturator(min_value=0, max_value=100)
+
+    # Test with input
+    out = saturator(50)
+    assert out == 50
+    out = saturator(-10)
+    assert out == 0
+    out = saturator(150)
+    assert out == 100
+
+    # Test with only max_value
+    saturator = Saturator(max_value=100)
+    out = saturator(150)
+    assert out == 100
+    out = saturator(-10)
+    assert out == -10
+
+    # Test with only min_value
+    saturator = Saturator(min_value=0)
+    out = saturator(-10)
+    assert out == 0
+    out = saturator(150)
+    assert out == 150
