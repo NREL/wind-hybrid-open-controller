b36f64be7206f9f05c58781efe35fc6685faf80c
misi9170 <michael.sinner@nlr.gov>
2026-06-17 11:54:34 -0600

    ruff formatting

--- hycon/controllers/__init__.py
+++ hycon/controllers/__init__.py
@@ -11,9 +11,9 @@ from hycon.controllers.lookup_based_wake_steering_controller import (
     LookupBasedWakeSteeringController,
 )
 from hycon.controllers.price_curtailing_controller import PriceCurtailingController
+from hycon.controllers.solar_controller import SolarController
 from hycon.controllers.solar_passthrough_controller import SolarPassthroughController
 from hycon.controllers.thermal_plant_controller import ThermalPlantController
-from hycon.controllers.solar_controller import SolarController
 from hycon.controllers.wake_steering_rosco_standin import WakeSteeringROSCOStandin
 from hycon.controllers.wind_farm_power_tracking_controller import (
     WindFarmPowerDistributingController,
